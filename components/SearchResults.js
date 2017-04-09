9bacd8bd7631b33954178f97af82dc97160c746b
jef4490 <jeffrey.hechler@gmail.com>
2017-04-09 12:56:13 -0400

    Done.

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -3,7 +3,15 @@
 import React from 'react';
 
 const SearchResults = ({ results }) => (
-  <ul>
+  <ul className="search-results">
+    {results.map((result) => {
+      return (<li>
+        <a href={result.link}>{result.title}</a>
+        <p>
+          {result.description}
+        </p>
+      </li>)
+    })}
   </ul>
 );
 
